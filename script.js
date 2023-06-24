e3e00ba144dd250265aabcbe54a6c27aa29f479e
Zahid Hasan <67813593+zhzahid1911509@users.noreply.github.com>
2023-06-24 09:06:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyvGXsbyt2MzFDD8jRCr1gWAGK7Oq3th1kwZF2XR-Dh1paALagLSvj0eqI6xwsTrZwumA/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
