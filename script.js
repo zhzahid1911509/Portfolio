f6c532166abafd3203f4bb99d3a6ad6ebca22b20
Zahid Hasan <67813593+zhzahid1911509@users.noreply.github.com>
2023-06-25 12:11:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -21,20 +21,25 @@ function closemenu(){
     sidemenu.style.right = "-200px";
 }
 
+
+
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyvGXsbyt2MzFDD8jRCr1gWAGK7Oq3th1kwZF2XR-Dh1paALagLSvj0eqI6xwsTrZwumA/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById('msg')
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => {msg.innerHTML = "Message Sent Successfully"
         setTimeout(function(){
             msg.innerHTML=""
-        },5000)
+        },3000)  
         form.reset()
+        
     })
       .catch(error => console.error('Error!', error.message))
   })
+
+
 
   ScrollReveal({
     reset: true,
